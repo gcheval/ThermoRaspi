1850388ac47686c0113dfacecad7469a7932365f
gcheval <chevalier321@gmail.com>
2014-07-27 13:11:38 -0400

    base UI (in dev)

--- ThermoRaspi/src/gchevalier/ThermoRaspi.java
+++ ThermoRaspi/src/gchevalier/ThermoRaspi.java
@@ -1,26 +1,80 @@
 package gchevalier;
 
+import java.awt.Font;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class ThermoRaspi extends JPanel{
 	private static final long serialVersionUID = 1L;
-	
-	public ThermoRaspi(JFrame frame){
-		super(new GridBagLayout());
-		JOptionPane.showMessageDialog(frame.getContentPane(), "It works!!");
-		
+	private JLabel currentTempLabel;
+	private JButton plusButton, minusButton;
+	JFrame parent;
+	public ThermoRaspi(JFrame parent){
+		this.parent = parent;
+		initWindow();
 	}
 	
 	private void initWindow(){
+		currentTempLabel = new JLabel("20");
+		plusButton = new JButton("+"); minusButton = new JButton("-");
+		this.parent.add(this);
+		this.parent.getContentPane().setLayout(new GridBagLayout());
+		this.parent.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 2;
+		c.gridy = 2;
+		c.gridheight = 1;
+		c.gridwidth = 2;
+		c.weightx = 1;
+		c.weighty = 1;
+		c.insets = new Insets(0,0,0,0);
+		currentTempLabel.setFont(new Font("serif", Font.PLAIN, 500));
+		this.add(currentTempLabel,c);
+		
+		c = new GridBagConstraints();
+		c.gridx = 1;
+		c.gridy = 1;
+		c.gridheight = 1;
+		c.gridwidth = 1;
+		c.weightx = 1;
+		c.weighty = 1;
+		c.insets = new Insets(0,0,0,0);
+		this.add(plusButton,c);
+		
+		c = new GridBagConstraints();
+		c.gridx = 1;
+		c.gridy = 1;
+		c.gridheight = 1;
+		c.gridwidth = 1;
+		c.weightx = 1;
+		c.weighty = 1;
+		c.insets = new Insets(0,0,0,0);
+		this.add(minusButton,c);
+		
+//		this.setMinimumSize(new Dimension());
+		this.parent.pack();
+		this.parent.setVisible(true);
+	}
+	
+	private void plusButton_Action(ActionEvent evt){
+		
+	}
+	private void minusButton_Action(ActionEvent evt){
 		
 	}
 	
 	public static void main (String[] args){
-		new ThermoRaspi(new JFrame());
+		new ThermoRaspi(new JFrame("Thermo2000"));
 	}
+	
 
 }
