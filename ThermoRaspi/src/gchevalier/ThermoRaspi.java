fc98f85532800e2d34f918c53d3367f58dc44d0a
gcheval <chevalier321@gmail.com>
2014-08-10 10:30:02 -0400

    new UI

--- ThermoRaspi/src/gchevalier/ThermoRaspi.java
+++ ThermoRaspi/src/gchevalier/ThermoRaspi.java
@@ -1,5 +1,6 @@
 package gchevalier;
 
+import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
@@ -16,124 +17,29 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JToggleButton;
 
-public class ThermoRaspi extends JPanel{
+public class ThermoRaspi{
 	private static final long serialVersionUID = 1L;
-	private JLabel currentTempLabel;
-	private JButton plusButton, 
-	minusButton,
-	exitButton;
 	JToggleButton onOffButton;
-	JFrame parent;
-	public ThermoRaspi(JFrame parent){
-		super();
-		this.parent = parent;
-		initWindow();
-	}
 	
-	private void initWindow(){
-		currentTempLabel = new JLabel("20");
-		plusButton = new JButton("+"); minusButton = new JButton("-");
-		exitButton = new JButton("Exit");
-		this.parent.add(this);
-		this.setLayout(new GridBagLayout());
-		this.parent.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		GridBagConstraints c = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 0;
-		c.gridheight = 4;
-		c.gridwidth = 2;
-		c.weightx = 1;
-		c.weighty = 1;
-		c.insets = new Insets(0,0,0,0);
-		c.fill = GridBagConstraints.BOTH;
-		c.anchor = GridBagConstraints.WEST;
-		currentTempLabel.setFont(new Font("serif", Font.PLAIN, 600));
-		this.add(currentTempLabel,c);
-		
-		c = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 4;
-		c.gridheight = 1;
-		c.gridwidth = 1;
-		c.weightx = 1;
-		c.weighty = 1;
-		c.fill = GridBagConstraints.BOTH;
-		c.anchor = GridBagConstraints.CENTER;
-		c.insets = new Insets(0,0,0,0);
-		this.add(plusButton,c);
-		plusButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				plusButton_Action(e);
-			}			
+	public ThermoRaspi( ){
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ThermoWindow frame = new ThermoWindow();
+					frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+					frame.setUndecorated(true);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
 		});
-		
-		c = new GridBagConstraints();
-		c.gridx = 1;
-		c.gridy = 4;
-		c.gridheight = 1;
-		c.gridwidth = 1;
-		c.weightx = 1;
-		c.weighty = 1;
-		c.fill = GridBagConstraints.BOTH;
-		c.anchor = GridBagConstraints.CENTER;
-		c.insets = new Insets(0,0,0,0);
-		this.add(minusButton,c);
-		minusButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				minusButton_Action(e);
-			}			
-		});
-		
-		c = new GridBagConstraints();
-		c.gridx = 1;
-		c.gridy = 0;
-		c.gridheight = 1;
-		c.gridwidth = 1;
-		c.weightx = 1;
-		c.weighty = 1;
-		c.fill = GridBagConstraints.BOTH;
-		c.anchor = GridBagConstraints.CENTER;
-		c.insets = new Insets(0,0,0,0);
-		this.add(exitButton,c);
-		exitButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				exitButton_Action(e);
-			}			
-		});
-		
-		c = new GridBagConstraints();
-		c.gridx = 1;
-		c.gridy = 1;
-		c.gridheight = 1;
-		c.gridwidth = 1;
-		c.weightx = 1;
-		c.weighty = 1;
-		c.fill = GridBagConstraints.BOTH;
-		c.anchor = GridBagConstraints.CENTER;
-		c.insets = new Insets(0,0,0,0);
-		this.add(new JPanel(),c);
-		this.parent.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-		this.parent.setUndecorated(true);
-		this.parent.pack();
-		this.parent.setVisible(true);
 	}
 	
 	private void plusButton_Action(ActionEvent evt){
 	}
 	private void minusButton_Action(ActionEvent evt){
 		
-	}
-	private void exitButton_Action(ActionEvent evt){
-		close();
-	}
-	
-	private void close(){
-		System.exit(0);
-	}
-	
-	public static void main (String[] args){
-		new ThermoRaspi(new JFrame("Thermo2000"));
 	}
 	
 
--- ThermoRaspi/src/gchevalier/ThermoWindow.java
+++ ThermoRaspi/src/gchevalier/ThermoWindow.java
@@ -9,6 +9,19 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import java.awt.GridBagLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
+import javax.swing.border.MatteBorder;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class ThermoWindow extends JFrame {
 	private JLabel currentTempLabel;
 	private JButton plusButton, 
@@ -20,29 +33,60 @@ public class ThermoWindow extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ThermoWindow frame = new ThermoWindow();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+	public ThermoWindow(){
+		setResizable(false);
+		setAlwaysOnTop(true);
+		initWindow();
 	}
 
 	/**
 	 * Create the frame.
+	 * @return 
 	 */
-	public ThermoWindow() {
+	private void initWindow(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JPanel panelCurrentTemp = new JPanel();
+		contentPane.add(panelCurrentTemp);
+		panelCurrentTemp.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblCurrentTemp = new JLabel("20");
+		lblCurrentTemp.setFont(new Font("Helvetica Neue", Font.PLAIN, 99));
+		panelCurrentTemp.add(lblCurrentTemp);
+		
+		JLabel lblNewLabel = new JLabel("<html><sup>°C</sup></html>");
+		lblNewLabel.setFont(new Font("Helvetica Neue", Font.PLAIN, 60));
+		panelCurrentTemp.add(lblNewLabel);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		contentPane.add(panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0};
+		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JButton btnNewButton = new JButton("Exit");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton.gridx = 0;
+		gbc_btnNewButton.gridy = 0;
+		panel.add(btnNewButton, gbc_btnNewButton);
 	}
+	
+
 
 }
